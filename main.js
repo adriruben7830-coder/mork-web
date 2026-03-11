78ecdef59f9f3236ffe62017fe80a79d246b4355
adriruben7830-coder <adriruben7830@gmail.com>
2026-03-11 14:28:55 +0100

    cursor interactivo en elementos clicables

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const cursor = document.querySelector('.cursor');
 
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX - 6 + 'px';
-    cursor.style.top = e.clientY - 6 + 'px';
+document.addEventListener('mousemove', e => {
+  cursor.style.left = e.clientX - 6 + 'px';
+  cursor.style.top = e.clientY - 6 + 'px';
 });
+
+const clicables = document.querySelectorAll('a, .servicio');
+
+clicables.forEach((elemento) => {
+    elemento.addEventListener('mouseenter', () => {
+        cursor.style.transform = 'scale(3)';
+        cursor.style.bordercolor = '#ffffff';
+    });
+    elemento.addEventListener('mouseleave', () => {
+        cursor.style.transform = 'scale(1)';
+        cursor.style.bordercolor = '#ffffff';
+    });
+});
+
